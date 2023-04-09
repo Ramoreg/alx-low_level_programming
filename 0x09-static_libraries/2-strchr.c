10aa14cd6d83027cc1cd0e7bee45713b673e7b85
Jimmy Abadi <jimmyabadi007@gmail.com>
2023-04-09 06:50:36 -0700

    Library updated

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
+
+
+
+
+ 
+============================
+ 
+2-strlen.c
+
+
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+
+	return (longi);
+}
